f23ae141c9a622273a8e4d0f86aa06f20b1b217c
인건 <kun@Kuns-MacBookPro.local>
2019-01-11 21:49:33 +0900

    email disabled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -76,6 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
   ActionMailer::Base.smtp_settings = {
       :user_name => ' postmaster@hufsgraduation.info',
       :password => 'bb9b491c87beeeb69a2de2d26dc31bcf-060550c6-cab2871f',
