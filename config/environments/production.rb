88fd2034550d61d98936581d7e7fd36a40425637
인건 <kun@Kuns-MacBookPro.local>
2019-01-12 22:31:21 +0900

    hide password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
       :domain               => 'mail.hufsgraduation.info',
       :port                 => 587,
       :user_name            => "post@hufsgraduation.info",
-      :password             => "daniel0324",
+      :password             => "ENV["EMAIL_PASSWORD"]",
       :authentication       => 'login',
       :enable_starttls_auto => true
   }
